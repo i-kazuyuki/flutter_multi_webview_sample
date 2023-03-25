8d3ce15f71e46e555dd5050573b126e8ebd33836
kazuyuki <ishijima@radicode.co.jp>
2023-03-25 15:40:18 +0900

    feat: ✨ add infinite_scroll_pagination sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
@@ -31,20 +32,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> _items = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+  final PagingController<int, String> _pagingController =
+  PagingController(firstPageKey: 0);
 
-  void _addItem() {
-    var newItemList = [..._items, _generateString()];
-    setState(() {
-      _items = newItemList;
-    });
-  }
-
-  void _removeItem(int index) {
-    var tmp = _items;
-    tmp.removeAt(index);
-    setState(() {
-      _items = tmp;
+  @override
+  void initState() {
+    super.initState();
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchData(pageKey);
     });
   }
 
@@ -55,42 +50,66 @@ class _MyHomePageState extends State<MyHomePage> {
         List.generate(length, (index) => random.nextInt(26) + 65));
   }
 
+  Future<void> _fetchData(int pageKey) async {
+    try {
+      final List<String> newData = [];
+      for(var i=0; i<10; i++){
+        newData.add(_generateString());
+      }
+      final isLastPage = newData.isEmpty;
+      if (isLastPage) {
+        _pagingController.appendLastPage([]);
+      } else {
+        final nextPageKey = pageKey + 1;
+        _pagingController.appendPage(newData, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        itemCount: _items.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: [
-              ListTile(
-                title: Text(_items[index]),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () => _removeItem(index),
+      body: PagedListView<int, String>(
+        pagingController: _pagingController,
+        builderDelegate: PagedChildBuilderDelegate<String>(
+          itemBuilder: (context, item, index) {
+            return Column(
+              children: [
+                ListTile(
+                  title: Text(item),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      _pagingController.itemList?.removeAt(index);
+                      _pagingController.refresh();
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 500,
-                height: 150,
-                child: Card(
-                  child: InAppWebView(
-                    initialUrlRequest: URLRequest(url: Uri.parse("https://flutter.dev/"))
-                  )
-                )
-              ),
-            ],
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addItem,
-        tooltip: 'Add Item',
-        child: const Icon(Icons.add),
+                SizedBox(
+                  width: 500,
+                  height: 150,
+                  child: Card(
+                    child: InAppWebView(
+                        initialUrlRequest: URLRequest(
+                            url: Uri.parse("https://flutter.dev/"))),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
   }
 }
